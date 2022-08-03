fa1fd7f8f09d2b13f7ee3f50e217ae0fbb2da1d8
Gabriel Camilo <gbl2000.lord@gmail.com>
2022-08-03 00:03:16 -0300

    Add files via upload

---
+++ Controllers/PokeFusionAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace POKEFUSION_CSHARP_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokeFusionAPIController : ControllerBase
+    {
+
+        private readonly ILogger<PokeFusionAPIController> _logger;
+
+        public PokeFusionAPIController(ILogger<PokeFusionAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<PokeFusionAPI> GetAsync()
+        {
+            try
+            {
+
+
+                using (var clientHttp = new HttpClient())
+                {
+                    var response = await clientHttp.GetStringAsync(@"https://pokemon.alexonsager.net");
+
+                    string pokename = response.Split(new[] { "pk_name\">" }, StringSplitOptions.None)[1];
+                    pokename = pokename.Substring(0, pokename.IndexOf("<"));
+
+                    string pokeIMg = response.Split(new[] { "pk_img\"" }, StringSplitOptions.None)[1];
+                    pokeIMg = pokeIMg.Substring(26, pokeIMg.IndexOf(">"));
+                    pokeIMg = pokeIMg.Substring(0, pokeIMg.IndexOf(">"));
+                    pokeIMg = pokeIMg.Trim();
+                    pokeIMg = pokeIMg.Substring(0, pokeIMg.Length - 1);
+
+                    return new PokeFusionAPI { pk_name = pokename, pk_img = pokeIMg };
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return new PokeFusionAPI { pk_name = e.Message, pk_img = "https://www.freeiconspng.com/uploads/error-icon-32.png" };
+            }
+        }
+    }
+}
---
+++ PokeFusionAPI.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace POKEFUSION_CSHARP_API
+{
+    public class PokeFusionAPI
+    {
+        public string pk_name { get; set; }
+
+        public string pk_img { get; set; }
+    }
+
+}
